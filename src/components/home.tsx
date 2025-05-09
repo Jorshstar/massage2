12e92372cd5c49ab416666e5fa6a92b7ed7fd24f
Jak-technology <ajosh22.aj@gmail.com>
2025-05-09 08:54:00 +0100

    just worked on

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -508,7 +508,7 @@ const Home = () => {
                       d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                     />
                   </svg>
-                  <span>info@relaxmassage.com</span>
+                  <span>info@marlenewellnessmassage.com</span>
                 </p>
                 
               </div>
